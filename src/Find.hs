10061481357d749679be78b77ac1d727656c8831
ZedPea <z.palmer@openmailbox.org>
2017-06-27 01:09:20 +0100

    Improve find code

--- src/Find.hs
+++ src/Find.hs
@@ -4,30 +4,32 @@ module Find
 )
 where
 
-import Control.Exception
-import Data.List (tails, stripPrefix, isPrefixOf)
-import Data.Maybe
-import Data.Char
 import Utilities 
 import Strings
+
+import Control.Exception
+import Data.List hiding (find)
+import Data.Maybe
+import Data.Char
 
 --We use &mincount=1 to add the smaller tags as well as the more popular ones
 find :: String -> IO (Either String [String])
 find searchTerm'
     | null searchTerm = return $ Left invalidTag
     | otherwise = do
-        eitherPage <- try $ openURL url :: IO (Either SomeException String)
-        case eitherPage of
-            Left _ -> return $ Left noInternet
-            Right page -> do
-                let tags = filter (searchTerm `isPrefixOf`) $ getTags page
-                case tags of
-                    [] -> return $ Left noTags
-                    _ -> return $ Right tags
+        eitherPage <- try $ openURL url
+        return $ findTags searchTerm eitherPage
     where searchTerm = scrub $ map toLower searchTerm'
           firstChar = head searchTerm
           baseURL = "http://rule34.paheal.net/tags?starts_with="
           url = baseURL ++ [firstChar] ++ "&mincount=1"
+          
+findTags :: String -> Either SomeException String -> Either String [String]
+findTags _ (Left _) = Left noInternet
+findTags searchTerm (Right page)
+    | null tags = Left noTags
+    | otherwise = Right tags
+    where tags = filter (searchTerm `isPrefixOf`) $ getTags page
 
 {- list/ is immediately before the tag name in the string we extracted earlier
 then we take until the next / which terminates the tag -}
@@ -38,13 +40,11 @@ isolate page = takeWhile (/= '/') start
 --Gets the text remaining in the string after the searchTerm
 myDrop :: String -> String -> String
 myDrop searchTerm soup
-    | null maybeEnd = []
+    | null maybeEnd = ""
     | otherwise = fromJust . stripPrefix searchTerm $ head maybeEnd
     where tails' = tails soup
           maybeEnd = filter (searchTerm `isPrefixOf`) tails'
 
-{- All lines containing a tag are prefixed with the below magic string
-We also lower case it all so case sensitivity in searching is no issue -}
 getTags :: String -> [String]
-getTags soup = [f x | x <- lines soup, "&nbsp;" `isPrefixOf` x]
-    where f = map toLower . removeEscapeSequences . isolate
+getTags soup = map cleanup $ filter ("&nbsp;" `isPrefixOf`) $ lines soup
+    where cleanup = map toLower . removeEscapeSequences . isolate
